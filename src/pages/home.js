39f649bb6e2ba216369bea86a1d2e74c4feeb4fd
PolianaVital <polianavital024@gmail.com>
2024-05-19 17:16:43 -0300

    continuacao

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,6 @@
 
 import Carousel from 'react-bootstrap/Carousel';
 import React from 'react';
-import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
 function HomePage(){
@@ -9,14 +8,14 @@ function HomePage(){
     return (
 
         <>
-
+        
          <Carousel data-bs-theme="dark">
-      <Carousel.Item>
-
-      <div className='main col-sm-12 col-md-6 col-lg-6'>
+         <Carousel.Item>
+          <div>
           <video src='https://media.w3.org/2010/05/sintel/trailer_hd.mp4' autoPlay loop muted></video>
-        </div>
-
+          </div>
+          
+     
         <Carousel.Caption>
         <font color="white">
         <h5 >Filmes</h5>
@@ -24,14 +23,24 @@ function HomePage(){
         </Carousel.Caption>
       </Carousel.Item>
 
-      
+      <Carousel.Item>
+      <img
+          className="d-block w-100"
+          src ={'https://miro.medium.com/v2/resize:fit:1200/1*wp7gvRlw2nw-_Nb5x901zg.png'}
+          alt="Filmes"
+        />
+        <Carousel.Caption>
+        <font color="white">
+        <h5 >Monstros S.A</h5>
+        </font>
+        </Carousel.Caption>
+      </Carousel.Item>
 
+      
       <Carousel.Item>
         <img
-        width={300}
-        height={800}
           className="d-block w-100"
-          src ={'https://miro.medium.com/v2/resize:fit:10240/1*LCZNGw2pS9VUxJmqUAzYHg.jpeg'}
+          src ={'https://www.amorporlivros.com.br/wp-content/uploads/livros-populares-2021.jpg'}
           alt="Livros"
         />
         <Carousel.Caption>
@@ -41,6 +50,7 @@ function HomePage(){
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
+    
 
     <div className='row boder pt-2'></div>
       <b>TOP FILMES</b>
@@ -48,13 +58,12 @@ function HomePage(){
 
       <div >
   
-        <div className='content mb-2 col-sm-12 col-md-6 col-lg-6 pt-2 d-flex gap-5'>
+        <div className='content mb-2 col-sm-2 col-md-3 col-lg-12 pt-2 d-flex gap-5'>
 
         <Card className='border md-4 col-sm-12 col-md-6 col-lg-6' style={{ width: '12rem' }}>
       <Card.Img variant="top" src="https://br.web.img3.acsta.net/pictures/16/09/12/22/13/415370.jpg" />
       <Card.Body>
         <Card.Title className='align-middle'>Moana</Card.Title>
-        <Button className='align-middle' variant="">Acessar</Button>
       </Card.Body>
     </Card>
 
@@ -62,7 +71,6 @@ function HomePage(){
       <Card.Img variant="top" src="https://m.media-amazon.com/images/I/91EKyuECxDL._AC_UF1000,1000_QL80_.jpg" />
       <Card.Body>
         <Card.Title className='align-middle'>Encanto</Card.Title>
-        <Button className='align-middle' variant="">Acessar</Button>
       </Card.Body>
     </Card>
 
@@ -70,7 +78,6 @@ function HomePage(){
       <Card.Img variant="top" src="https://lumiere-a.akamaihd.net/v1/images/daylight_payoff_poster_brazil_27eb2c00.jpeg" />
       <Card.Body>
         <Card.Title className='align-middle'>Wish</Card.Title>
-        <Button className='align-middle' variant="">Acessar</Button>
       </Card.Body>
     </Card>
 
@@ -78,7 +85,6 @@ function HomePage(){
       <Card.Img variant="top" src="https://lumiere-a.akamaihd.net/v1/images/image_b0bdb13a.jpeg?region=0%2C0%2C540%2C810&width=320" />
       <Card.Body>
         <Card.Title className='align-middle'>Divertidamente 2 </Card.Title>
-        <Button className='align-middle' variant="">Acessar</Button>
       </Card.Body>
     </Card>
 
@@ -86,7 +92,6 @@ function HomePage(){
       <Card.Img variant="top" src="https://apostiladecinema.com.br/wp-content/uploads/2021/06/luca-filme-pixar-animacao-disney-2021-critica-poster-scaled-e1629396654805.jpg" />
       <Card.Body>
         <Card.Title className='align-middle'>Luca</Card.Title>
-        <Button className='align-middle' variant="">Acessar</Button>
       </Card.Body>
     </Card>
 
@@ -94,7 +99,6 @@ function HomePage(){
       <Card.Img variant="top" src="https://lh4.googleusercontent.com/xbryw1wWK0SAxsKaNRX7QnMcaTWTZg9s24y_g-XtpPtOTNE6I5MWN_6rLrBgokcPYawGYMR8QMGurel-E4k5sn2qnjiV7Ru9ulQuBowuC3aUhbc0Y8FOzrnQGZRA-94wYr44ja1XH6M2306IDg" />
       <Card.Body>
         <Card.Title className='align-middle'>A fantástica fábrica de chocolate</Card.Title>
-        <Button className='align-middle' variant="">Acessar</Button>
       </Card.Body>
     </Card>
 
@@ -102,7 +106,6 @@ function HomePage(){
       <Card.Img variant="top" src="https://br.web.img3.acsta.net/medias/nmedia/18/91/05/36/20127436.jpg" />
       <Card.Body>
         <Card.Title className='align-middle'>Toy Story</Card.Title>
-        <Button className='align-middle' variant="">Acessar</Button>
       </Card.Body>
     </Card>
 
@@ -110,7 +113,6 @@ function HomePage(){
       <Card.Img variant="top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJwPKnv5ZSM5JNAA1AYQQTRZbwmrh0Cm0zE-sBZcrqzw&s" />
       <Card.Body>
         <Card.Title className='align-middle'>Monstros S.A</Card.Title>
-        <Button className='align-middle' variant="">Acessar</Button>
       </Card.Body>
     </Card>
 
@@ -126,7 +128,6 @@ function HomePage(){
       <Card.Img variant="top" src="https://m.media-amazon.com/images/I/719esIW3D7L._AC_UF1000,1000_QL80_.jpg" />
       <Card.Body>
         <Card.Title className='align-middle'>Orgulho e preconceito</Card.Title>
-        <Button className='align-middle' variant="">Acessar</Button>
       </Card.Body>
     </Card>
 
@@ -134,7 +135,6 @@ function HomePage(){
       <Card.Img variant="top" src="https://m.media-amazon.com/images/I/A1UjcPz4gZL._AC_UF1000,1000_QL80_.jpg" />
       <Card.Body>
         <Card.Title className='align-middle'>Percy Jackson e os olimpianos</Card.Title>
-        <Button className='align-middle' variant="">Acessar</Button>
       </Card.Body>
     </Card>
 
@@ -142,7 +142,6 @@ function HomePage(){
       <Card.Img variant="top" src="https://m.media-amazon.com/images/I/51fMZPtkW+L.jpg" />
       <Card.Body>
         <Card.Title className='align-middle'>Uma segunda chance</Card.Title>
-        <Button className='align-middle' variant="">Acessar</Button>
       </Card.Body>
     </Card>
 
@@ -150,7 +149,6 @@ function HomePage(){
       <Card.Img variant="top" src="https://osmelhoreslivros.com.br/wp-content/uploads/2021/04/corte-de-nevoa-e-furia-202x300.jpg" />
       <Card.Body>
         <Card.Title className='align-middle'>Corte de névoa e fúria</Card.Title>
-        <Button className='align-middle' variant="">Acessar</Button>
       </Card.Body>
     </Card>
 
@@ -158,7 +156,6 @@ function HomePage(){
       <Card.Img variant="top" src="https://m.media-amazon.com/images/I/71SYepTUsDL._AC_UF1000,1000_QL80_.jpg" />
       <Card.Body>
         <Card.Title className='align-middle'>Vermelho, branco e sangue azul</Card.Title>
-        <Button className='align-middle' variant="">Acessar</Button>
       </Card.Body>
     </Card>
 
@@ -166,7 +163,6 @@ function HomePage(){
       <Card.Img variant="top" src="https://m.media-amazon.com/images/I/61GFsO7j0ZL._AC_UF1000,1000_QL80_DpWeblab_.jpg" />
       <Card.Body>
         <Card.Title className='align-middle'>Um estudo em vermelho</Card.Title>
-        <Button className='align-middle' variant="">Acessar</Button>
       </Card.Body>
     </Card>
 
@@ -174,7 +170,6 @@ function HomePage(){
       <Card.Img variant="top" src="https://m.media-amazon.com/images/I/715+3WHXWyL._AC_UF1000,1000_QL80_.jpg" />
       <Card.Body>
         <Card.Title className='align-middle'>Quem é você Alasca? </Card.Title>
-        <Button className='align-middle' variant="">Acessar</Button>
       </Card.Body>
     </Card>
 
@@ -182,7 +177,6 @@ function HomePage(){
       <Card.Img variant="top" src="https://m.media-amazon.com/images/I/51M9IbBqxCL._AC_UF1000,1000_QL80_.jpg" />
       <Card.Body>
         <Card.Title className='align-middle'>A culpa é das estrelas</Card.Title>
-        <Button className='align-middle' variant="">Acessar</Button>
       </Card.Body>
     </Card>
 
